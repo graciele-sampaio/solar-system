068a9c29085efe10ec234ab18a5c5974cf32591f
Graciele <graciele@pop-os.localdomain>
2022-06-28 13:51:31 -0300

    css inicial(bem inicial)

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -8,7 +8,7 @@ class Missions extends React.Component {
     return (
       <div data-testid="missions">
         <Title headline="Missões" />
-        <section>
+        <section className="missions">
           {missions.map((mission) => (
             <MissionCard
               name={ mission.name }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -8,7 +8,7 @@ class SolarSystem extends React.Component {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
-        <section>
+        <section className="planets">
           {planets.map((planet) => (
             <PlanetCard
               planetName={ planet.name }
